611ca8d150f129ea8a94ecc1c90874818f58c66f
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-13 11:12:26 +0200

    Add utility files

---
+++ StringR.Store/Utility/Utility.swift
@@ -0,0 +1,16 @@
+//
+//  Utility.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 13/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+
+class Utility {
+    
+    // generel utility goes here..
+    
+    
+}
