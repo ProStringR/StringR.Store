c09d11e716b30dfaefe769ba9be6e482f2cca555
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-13 12:58:20 +0200

    create shop & address objects

---
+++ StringR.Store/Model/Address.swift
@@ -0,0 +1,19 @@
+//
+//  Address.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 13/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+
+struct Address {
+
+    private var street: String
+    private var zipCode: String
+    private var city: String
+    private var country: String
+    private var longitude: Double
+    private var latitude: Double
+}
---
+++ StringR.Store/Model/Shop.swift
@@ -0,0 +1,20 @@
+//
+//  Shop.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 13/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+
+struct Shop {
+
+    private var shopId: String
+    private var address: Address
+    private var phoneNumber: String
+    private var storageId: String
+    private var teamId: String
+    private var conomyId: String
+
+}
