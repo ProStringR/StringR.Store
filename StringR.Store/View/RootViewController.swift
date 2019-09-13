02f454cbf5709b5afcf9b6b030e042dcc1811144
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-13 10:56:27 +0200

    remove storyboard

--- StringR.Store/View/RootViewController.swift
+++ StringR.Store/View/RootViewController.swift
@@ -1,20 +1,21 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  StringR.Store
 //
 //  Created by Marcus Christiansen on 13/09/2019.
 //  Copyright © 2019 StringR. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class RootViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        // Fun here!
+        
     }
-
-
 }
-
