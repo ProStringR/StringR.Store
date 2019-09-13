64970489ada647b80f272ac733f181d2a41b7206
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-13 11:51:29 +0200

    swiftLint setup

--- StringR.Store/AppDelegate.swift
+++ StringR.Store/AppDelegate.swift
@@ -13,14 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = RootViewController()
         window?.makeKeyAndVisible()
-        
+
         return true
     }
 
@@ -46,6 +44,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
--- StringR.Store/View/RootViewController.swift
+++ StringR.Store/View/RootViewController.swift
@@ -10,12 +10,9 @@ import Foundation
 import UIKit
 
 class RootViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .white
-        // Fun here!
-        
     }
 }
