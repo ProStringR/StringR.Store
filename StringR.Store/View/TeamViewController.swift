d8c8f7c1394118b4f4de6e1fade390d6366f997c
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-13 16:43:07 +0200

    Add viewControllers

---
+++ StringR.Store/View/TeamViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TeamViewController.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 13/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TeamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ StringR.Store/View/TestRacketViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TestRacketViewController.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 13/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TestRacketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
