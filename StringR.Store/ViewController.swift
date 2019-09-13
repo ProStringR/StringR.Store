31076be65ca607185b2f3c7ded3daca78034ce97
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-13 09:11:17 +0200

    Initial commit

---
+++ StringR.Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 13/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
