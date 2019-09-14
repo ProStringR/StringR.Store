58f7467beeba78f70606c7a7d3faf4945df46337
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-14 13:39:14 +0200

    Add extension to hide nav bar

---
+++ StringR.Store/Utility/Extention.swift
@@ -0,0 +1,19 @@
+//
+//  Extention.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 14/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationController {
+    func hideNavigationBar() {
+        self.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationBar.shadowImage = UIImage()
+        self.navigationBar.isTranslucent = true
+        self.view.backgroundColor = UIColor.clear
+    }
+}
