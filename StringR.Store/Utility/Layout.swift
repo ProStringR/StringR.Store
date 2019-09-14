1514dc494f988ee543ed4dd5e8346c4e4ff3180b
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-14 18:36:55 +0200

    create layout file with layout funcs

---
+++ StringR.Store/Utility/Layout.swift
@@ -0,0 +1,50 @@
+//
+//  Layout.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 14/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Layout {
+
+    static func centerVertically(on view: UIView, withParent parentView: UIView) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.centerYAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.centerYAnchor).isActive = true
+    }
+
+    static func centerHorizontally(on view: UIView, withParent parentView: UIView) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.centerXAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.centerXAnchor).isActive = true
+    }
+
+    static func addTopConstraint(on view: UIView, to anchor: NSLayoutYAxisAnchor, by points: CGFloat) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.topAnchor.constraint(equalTo: anchor, constant: points).isActive = true
+    }
+
+    static func addBottomConstraint(on view: UIView, to anchor: NSLayoutYAxisAnchor, by points: CGFloat) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.bottomAnchor.constraint(equalTo: anchor, constant: points).isActive = true
+    }
+
+    static func addLeadingConstraint(on view: UIView, to anchor: NSLayoutXAxisAnchor, by points: CGFloat) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.leadingAnchor.constraint(equalTo: anchor, constant: points).isActive = true
+    }
+
+    static func addTrailingConstraint(on view: UIView, to anchor: NSLayoutXAxisAnchor, by points: CGFloat) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.trailingAnchor.constraint(equalTo: anchor, constant: points).isActive = true
+    }
+
+    static func addHeadingLayout(on view: UILabel, withParent parentView: UIView) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.font = UIFont.boldSystemFont(ofSize: 32)
+        centerHorizontally(on: view, withParent: parentView)
+        addTopConstraint(on: view, to: parentView.topAnchor, by: 75)
+    }
+}
