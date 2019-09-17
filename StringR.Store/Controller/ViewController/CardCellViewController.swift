b003d487d143b0deabf98e3b457079010493d010
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-09-17 15:37:53 +0200

    Create CardCellViewController.swift

---
+++ StringR.Store/Controller/ViewController/CardCellViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CardCellViewController.swift
+//  StringR.Store
+//
+//  Created by Jaafar on 17/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CardCellViewController: UIViewController {
+
+}
