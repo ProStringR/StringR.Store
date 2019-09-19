e06e2754c6ea5494061aef50ceff5c2eba7a9d90
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-19 14:49:42 +0200

    Create OrderDAOFirebase.swift

---
+++ StringR.Store/Controller/DAO/OrderDAOFirebase.swift
@@ -0,0 +1,27 @@
+//
+//  OrderDAO.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 19/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+
+class OrderDAOFirebase: OrderDAOProtocol {
+
+    func getActiveOrders() throws -> [OrderDTO] {
+        do {
+            var orders = [OrderDTO]()
+            let data = try DataController.fetchData(returnType: [String: OrderDTO].self, url: Firebase.order)
+
+            for order in data {
+                orders.append(order.value)
+            }
+
+            return orders
+        } catch {
+            throw error
+        }
+    }
+}
