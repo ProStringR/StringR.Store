b464a600b1c54f6fb9ab3e1acbd9ea9268c8b6e4
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-19 16:34:03 +0200

    Implement orderDAOFirebase

--- StringR.Store/Controller/DAO/OrderDAOFirebase.swift
+++ StringR.Store/Controller/DAO/OrderDAOFirebase.swift
@@ -13,13 +13,21 @@ class OrderDAOFirebase: OrderDAOProtocol {
     func getActiveOrders() throws -> [OrderDTO] {
         do {
             var orders = [OrderDTO]()
-            let data = try DataController.fetchData(returnType: [String: OrderDTO].self, url: Firebase.order)
+            let data = try DataController.getData(returnType: [String: OrderDTO].self, url: Firebase.order)
 
             for order in data {
                 orders.append(order.value)
             }
 
             return orders
+        } catch {
+            throw error
+        }
+    }
+
+    func postOrder(order: OrderDTO) throws {
+        do {
+            try DataController.postData(object: order, url: Firebase.order)
         } catch {
             throw error
         }
