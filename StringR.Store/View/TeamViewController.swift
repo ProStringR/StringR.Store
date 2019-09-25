81c63e0932f10a401a893b9e0438985b812b2c9f
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-25 15:58:02 +0200

    Create TableView on the team site

--- StringR.Store/View/TeamViewController.swift
+++ StringR.Store/View/TeamViewController.swift
@@ -11,14 +11,82 @@ import UIKit
 
 class TeamViewController: UIViewController {
 
+    weak var teamTableView: UITableView!
+    var stringers: [Stringer]?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setLayout()
+        setupTableView()
+        getStringers()
+        setupConstraints()
+    }
+
+    private func getStringers() {
+        // Call controller to get team data
+
+        // Store the team in the stringers array
+
+        // Reload the tableView
+        self.teamTableView.reloadData()
     }
 
     private func setLayout() {
         self.view.backgroundColor = .white
         Layout.setupViewNavigationController(forView: self, withTitle: Utility.getString(forKey: "generel_Team"))
+    }
+
+    private func setupTableView() {
+        let tableView = LayoutController.getTableView(cellType: UserCell.self, cellIdentifier: UserCell.identifier)
+        self.view.addSubview(tableView)
+        self.teamTableView = tableView
+
+        self.teamTableView.delegate = self
+        self.teamTableView.dataSource = self
+    }
+
+    private func setupConstraints() {
+        Layout.addTopConstraint(on: self.teamTableView, to: view.safeAreaLayoutGuide.topAnchor)
+        Layout.addBottomConstraint(on: self.teamTableView, to: view.safeAreaLayoutGuide.bottomAnchor)
+        Layout.addLeadingConstraint(on: self.teamTableView, to: view.safeAreaLayoutGuide.leadingAnchor)
+        Layout.addTrailingConstraint(on: self.teamTableView, to: view.safeAreaLayoutGuide.trailingAnchor)
+    }
+}
+
+extension TeamViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let team = self.stringers {
+            return team.count
+        }
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+//        guard let team = self.stringers else { return UITableViewCell() }
+
+        // swiftlint:disable force_cast
+        let cell = self.teamTableView.dequeueReusableCell(withIdentifier: UserCell.identifier, for: indexPath) as! UserCell
+        // swiftlint:enable force_cast
+
+//        let currentStringer = team[indexPath.row]
+//
+//        cell.nameLabel.text = currentStringer.name
+//        cell.phoneNumberLabel.text = currentStringer.phoneNumber
+
+        // This needs to replaced with the code above...
+        cell.nameLabel.text = "Marcus August Christiansen"
+        cell.phoneNumberLabel.text = "25722345"
+
+        cell.accessoryType = .detailButton
+        cell.tintColor = .black
+        return cell
+    }
+}
+
+extension TeamViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Do something
     }
 }
