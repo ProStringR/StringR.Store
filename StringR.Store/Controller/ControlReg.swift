4db455820c4a7fd3539622020283b8d7c7185284
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-30 15:49:24 +0200

    Create ControlReg.swift

---
+++ StringR.Store/Controller/ControlReg.swift
@@ -0,0 +1,20 @@
+//
+//  ControlReg.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 30/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+
+class ControlReg {
+
+    // DAO
+    static let getOrderDAO = OrderDAOFirebase()
+    static let getTeamDAO = TeamDAOFirebase()
+
+    // Control
+    static let getOrderController = OrderController()
+    static let getTeamController = TeamController()
+}
