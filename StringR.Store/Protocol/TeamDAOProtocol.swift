d17e1e5c6d33fdbad95fed35d12571bc052c97ae
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-30 15:50:45 +0200

    Create TeamDAOProtocol.swift

---
+++ StringR.Store/Protocol/TeamDAOProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  TeamDAOProtocol.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 30/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+
+protocol TeamDAOProtocol {
+    func postStringer(stringer: Stringer) throws
+}
