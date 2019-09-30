cb4c2c8cbe19309087399b4ddc5e2a647b9901a9
Marcus August Christiansen <s175185@student.dtu.dk>
2019-09-30 15:50:20 +0200

    Create StringerDTO.swift

---
+++ StringR.Store/Model/DTO/StringerDTO.swift
@@ -0,0 +1,37 @@
+//
+//  StringerDTO.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 30/09/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+
+class StringerDTO: Codable {
+    var userId: String?
+    var name: String?
+    var birthday: Int64?
+    var email: String?
+    var phoneNumber: String?
+    var address: Address?
+    var preferedRacketType: RacketType?
+    var orderIds: [String]?
+
+    init(userId: String, name: String, birthday: Int64, email: String, phoneNumber: String, address: Address, preferedRacketType: RacketType, orderIds: [String]) {
+        self.userId = userId
+        self.name = name
+        self.birthday = birthday
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.address = address
+        self.preferedRacketType = preferedRacketType
+        self.orderIds = orderIds
+    }
+
+    init?(stringer: Stringer) throws {
+        let json = try JSONEncoder().encode(stringer)
+        let newStringer = try JSONDecoder().decode(StringerDTO.self, from: json)
+
+    }
+}
