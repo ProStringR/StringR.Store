36c9e2eb984bd508c7a07f6ba918e5df69fde546
Marcus August Christiansen <s175185@student.dtu.dk>
2019-10-01 08:53:06 +0200

    Team DAO has get and put

--- StringR.Store/Controller/DAO/TeamDAOFirebase.swift
+++ StringR.Store/Controller/DAO/TeamDAOFirebase.swift
@@ -10,8 +10,25 @@ import Foundation
 
 class TeamDAOFirebase: TeamDAOProtocol {
 
+    let dataControl = ControlReg.getDataController
+
+    func getStringer(basedOn stringerId: String) -> StringerDTO? {
+        do {
+            let stringer = try dataControl.getData(returnType: StringerDTO.self, url: "\(Firebase.stringer)/\(stringerId)")
+            return stringer
+        } catch {
+            return nil
+        }
+    }
+
     func postStringer(stringer: Stringer) throws {
-        try DataController.postData(object: stringer, url: Firebase.stringer)
+        try dataControl.postData(object: stringer, url: Firebase.stringer)
+    }
+
+    func putStringer(stringerDTO: StringerDTO?) throws {
+        guard let stringerDTO = stringerDTO, let id = stringerDTO.userId else { throw Exception.nilPoint }
+
+        try dataControl.putData(objectToUpdate: stringerDTO, objectId: id, url: Firebase.stringer)
     }
 
 }
