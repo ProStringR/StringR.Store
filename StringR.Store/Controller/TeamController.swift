803f4a4b189da5a5646c727e4ea3a5e28cb3354c
Marcus August Christiansen <s175185@student.dtu.dk>
2019-10-01 08:51:39 +0200

    put and get stringer is now ready

--- StringR.Store/Controller/TeamController.swift
+++ StringR.Store/Controller/TeamController.swift
@@ -11,8 +11,29 @@ import Foundation
 class TeamController {
 
     let teamDAO: TeamDAOProtocol = ControlReg.getTeamDAO
+    let dataControl = ControlReg.getDataController
+
+    func getStringer(basedOn stringerId: String) -> Stringer? {
+        let dto = teamDAO.getStringer(basedOn: stringerId)
+
+        if let stringerDTO = dto {
+            return dataControl.createObject(fromObject: stringerDTO, toObject: Stringer.self)
+        } else {
+            return nil
+        }
+    }
 
     func postStringer(stringer: Stringer) throws {
         try teamDAO.postStringer(stringer: stringer)
+    }
+
+    /**
+     Put a stringer to the database. Uses the stringers id to place the stringer in the database.
+
+     - Parameter stringer: The stringer you want to put to the database.
+     */
+    func putStringer(stringer: Stringer) throws {
+        let stringerDTO = dataControl.createObject(fromObject: stringer, toObject: StringerDTO.self)
+        try teamDAO.putStringer(stringerDTO: stringerDTO)
     }
 }
