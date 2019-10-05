d8cab30909b94e93635c97c6ed6c1866aacba927
Marcus August Christiansen <s175185@student.dtu.dk>
2019-10-05 11:19:14 +0200

    getStringer and getTeam

--- StringR.Store/Controller/DAO/TeamDAOFirebase.swift
+++ StringR.Store/Controller/DAO/TeamDAOFirebase.swift
@@ -21,14 +21,46 @@ class TeamDAOFirebase: TeamDAOProtocol {
         }
     }
 
+    func getStringerTest(basedOn stringerId: String, completion: @escaping (StringerDTO?) -> Void) {
+        do {
+            try dataControl.getDataTest(returnType: StringerDTO.self, url: "\(Firebase.stringer)/\(stringerId)", completion: { (result) in
+                completion(result)
+            })
+        } catch {
+            completion(nil)
+        }
+    }
+
+    func getTeam(basedOn teamId: String) -> TeamDTO? {
+        do {
+            let team = try dataControl.getData(returnType: TeamDTO.self, url: "\(Firebase.team)/\(teamId)")
+            return team
+        } catch {
+            return nil
+        }
+    }
+
+    func getTeamTest(basedOn teamId: String, completion: @escaping (TeamDTO?) -> Void) {
+        do {
+            try dataControl.getDataTest(returnType: TeamDTO.self, url: "\(Firebase.team)/\(teamId)", completion: { (result) in
+                completion(result)
+            })
+        } catch {
+            completion(nil)
+        }
+    }
+
     func postStringer(stringer: Stringer) throws {
         try dataControl.postData(object: stringer, url: Firebase.stringer)
     }
 
     func putStringer(stringerDTO: StringerDTO?) throws {
         guard let stringerDTO = stringerDTO, let id = stringerDTO.userId else { throw Exception.nilPoint }
-
         try dataControl.putData(objectToUpdate: stringerDTO, objectId: id, url: Firebase.stringer)
     }
 
+    func putTeam(team: TeamDTO?) throws {
+        guard let team = team, let id = team.teamId else { throw Exception.nilPoint }
+        try dataControl.putData(objectToUpdate: team, objectId: id, url: Firebase.team)
+    }
 }
