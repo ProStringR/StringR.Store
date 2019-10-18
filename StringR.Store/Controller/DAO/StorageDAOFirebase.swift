99cd893acfb2d33494e3ebe173702dd0381caa5d
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-10-18 10:04:00 +0200

    Update StorageDAOFirebase.swift

--- StringR.Store/Controller/DAO/StorageDAOFirebase.swift
+++ StringR.Store/Controller/DAO/StorageDAOFirebase.swift
@@ -11,10 +11,21 @@ import Foundation
 class StorageDAOFirebase {
     let dataControl = ControlReg.getDataController
 
-    func putRacketString(racketString: RacketStringDTO?, completion: @escaping (Bool) -> Void) {
+    func putRacketString(racketString: RacketStringDTO?, url: String, completion: @escaping (Bool) -> Void) {
         guard let racketStringDTO = racketString, let id = racketString?.stringId else { completion(false); return}
-        dataControl.putData(objectToUpdate: racketStringDTO, objectId: id, url: Firebase.storage) { (succes) in
+        dataControl.putData(objectToUpdate: racketStringDTO, objectId: id, url: url) { (succes) in
             completion(succes)
+        }
+    }
+
+    func getStringsInStorage(basedOnShopId id: String, andStringId  completion: @escaping ([RacketStringDTO]?) -> Void) {
+        do {
+            try dataControl.getListOfData(returnType: RacketStringDTO.self, url: "\(Firebase.storage)/\(id)") { (resultArray) in
+                completion(resultArray)
+            }
+        } catch {
+            print("something went wrong", error)
+            completion(nil)
         }
     }
 }
