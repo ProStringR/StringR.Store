9e499be25033a37809abffa3f493c20f236fef63
Marcus August Christiansen <s175185@student.dtu.dk>
2019-10-19 17:15:28 +0200

    Get image indication from the RacketString

--- StringR.Store/Model/RacketString.swift
+++ StringR.Store/Model/RacketString.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class RacketString: Codable {
 
@@ -69,5 +70,16 @@ class RacketString: Codable {
 
     func getDescription() -> String {
         return "\(self.brand.rawValue) | \(self.modelName) | \(self.thickness)"
+    }
+
+    func getImageIndication() -> UIImage {
+        switch self.stringPurpose {
+        case .TENNIS:
+            return #imageLiteral(resourceName: "tennisball")
+        case .BADMINTON:
+            return #imageLiteral(resourceName: "shuttlecock")
+        case .SQUASH:
+            return #imageLiteral(resourceName: "squashball")
+        }
     }
 }
