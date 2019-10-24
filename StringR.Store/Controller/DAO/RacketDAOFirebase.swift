bd1cc7b772371392dd0e94e22fffc98fe69e90c6
Marcus August Christiansen <s175185@student.dtu.dk>
2019-10-24 16:10:49 +0200

    Get and put racket

---
+++ StringR.Store/Controller/DAO/RacketDAOFirebase.swift
@@ -0,0 +1,30 @@
+//
+//  RacketDAOFirebase.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 24/10/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+
+class RacketDAOFirebase: RacketDAOProtocol {
+
+    let dataControl = ControlReg.getDataController
+
+    func getRacket(racketId: String, completion: @escaping (Racket?) -> Void) {
+        dataControl.getData(returnType: RacketDTO.self, url: "\(Firebase.racket)/\(racketId)") { (result) in
+            completion(self.dataControl.createObject(fromObject: result, toObject: Racket.self))
+        }
+    }
+
+    func putRacket(racket: Racket, completion: @escaping (Bool) -> Void) {
+        let dto = self.dataControl.createObject(fromObject: racket, toObject: RacketDTO.self)
+
+        guard let object = dto, let id = dto?.racketId else { completion(false); return }
+
+        dataControl.putData(objectToUpdate: object, objectId: id, url: Firebase.racket) { (succes) in
+            completion(succes)
+        }
+    }
+}
