409a94d6dabdc50042d094ce5053c0a116ab22be
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-10-25 09:27:59 +0200

    Create OrderHistory.swift

---
+++ StringR.Store/Model/OrderHistory.swift
@@ -0,0 +1,21 @@
+//
+//  OrderHistory.swift
+//  StringR.Store
+//
+//  Created by Jaafar on 25/10/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+
+class OrderHistory: Codable {
+    var date: Int64
+    var paid: Bool
+    var orderStatus: OrderStatus
+
+    init(date: Int64, paid: Bool, orderStatus: OrderStatus) {
+        self.date = date
+        self.paid = paid
+        self.orderStatus = orderStatus
+    }
+}
