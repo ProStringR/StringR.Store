6fcb9f077aea7c77ad7a3d82c935d192c9704543
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-10-25 09:27:52 +0200

    Add view for order history

---
+++ StringR.Store/View/PopUp/SpecificOrderHistoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SpecificOrderHistoryViewController.swift
+//  StringR.Store
+//
+//  Created by Jaafar on 25/10/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import UIKit
+
+class SpecificOrderHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
