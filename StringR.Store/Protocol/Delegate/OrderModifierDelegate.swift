e47e5a051a01e1f3f040012352070c8791e28948
Marcus August Christiansen <s175185@student.dtu.dk>
2019-10-28 08:59:26 +0100

    Delegate has been created

---
+++ StringR.Store/Protocol/Delegate/OrderModifierDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  StringR.Store
+//
+//  Created by Marcus Christiansen on 25/10/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+
+protocol OrderModifierDelegate: class {
+    func orderHasBeenModified()
+}
