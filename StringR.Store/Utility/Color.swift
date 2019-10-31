c5194ebc64cffa78b3435df133110fdfe2337a23
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-10-31 11:33:46 +0100

    Add colors

---
+++ StringR.Store/Utility/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  StringR.Store
+//
+//  Created by Jaafar on 30/10/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Color {
+    static let darkGrey: UIColor = UIColor(red: 72/255, green: 72/255, blue: 74/255, alpha: 1)
+    static let lightGrey: UIColor = UIColor(red: 199/255, green: 199/255, blue: 204/255, alpha: 1)
+}
